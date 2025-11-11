2de8d512a81b04b13c5eea3839f0a1164dde8f2a
Isabelle Pinheiro Lovo <isabelle.pinheirolv@gmail.com>
2025-11-10 23:07:33 -0300

    edited

--- prjSCMFlusao/src/model/Produto.java
+++ prjSCMFlusao/src/model/Produto.java
@@ -1,5 +1,80 @@
 package model;
 
 public class Produto {
+
+    // ATRIBUTOS
+
+    private String id;
+    private String nome;
+    private String situacao;
+    private double preco;
+
+    // MÉTODOS PADRÃO
+
+    public Produto(String id, String nome, String situacao, double preco) {
+        this.setId(id);
+        this.setNome(nome);
+        this.setSituacao(situacao);
+        this.setPreco(preco);
+    }
     
+    // public Produto(?) { }
+
+    private void setId(String id) {
+        this.validarId(id);
+        this.id = id;
+    }
+
+    private String getId() {
+        return this.id;
+    }
+
+    private void setNome(String nome) {
+        this.validarNome(nome);
+        this.nome = nome;
+    }
+
+    private String getNome() {
+        return this.nome;
+    }
+
+    private void setSituacao(String situacao) {
+        this.validarSituacao(situacao);
+        this.situacao = situacao;
+    }
+
+    private String getSituacao() {
+        return this.situacao;
+    }
+
+    private void setPreco(double preco) {
+        this.validarPreco(preco);
+        this.preco = preco;
+    }
+
+    private double getPreco() {
+        return this.preco;
+    }
+
+    // MÉTODOS DOS DIAGRAMAS (D.E e D.S)
+
+
+    // MÉTODOS DE VALIDAÇÃO
+
+    private void validarId(String id) {
+
+    }
+
+    private void validarNome(String nome) {
+
+    }
+
+    private void validarSituacao(String situacao) {
+
+    }
+
+    private void validarPreco(double preco) {
+
+    }
+
 }
--- prjSCMFlusao/src/model/TransporteProduto.java
+++ prjSCMFlusao/src/model/TransporteProduto.java
@@ -1,5 +1,97 @@
 package model;
 
+import java.util.Date;
+
 public class TransporteProduto {
-    
+
+    // ATRIBUTOS
+
+    private String id;
+    private String situacao;
+    private Date dtSaida;
+    private Date dtChegada;
+    private double frete;
+
+    // MÉTODOS PADRÃO
+
+    public TransporteProduto(String id, String situacao, Date dtSaida, Date dtChegada, double frete) {
+        this.setId(id);
+        this.setSituacao(situacao);
+        this.setDtSaida(dtSaida);
+        this.setDtChegada(dtChegada);
+        this.setFrete(frete);
+    }
+
+    // public TransporteProduto(?) { }
+
+    private void setId(String id) {
+        this.validarId(id);
+        this.id = id;
+    }
+
+    private String getId() {
+        return this.id;
+    }
+
+    private void setSituacao(String situacao) {
+        this.validarSituacao(situacao);
+        this.situacao = situacao;
+    }
+
+    private String getSituacao() {
+        return this.situacao;
+    }
+
+    private void setDtSaida(Date dtSaida) {
+        this.validarDtSaida(dtSaida);
+        this.dtSaida = dtSaida;
+    }
+
+    private Date getDtSaida() {
+        return this.dtSaida;
+    }
+
+    private void setDtChegada(Date dtChegada) {
+        this.validarDtChegada(dtChegada);
+        this.dtChegada = dtChegada;
+    }
+
+    private Date getDtChegada() {
+        return this.dtChegada;
+    }
+
+    private void setFrete(double frete) {
+        this.validarFrete(frete);
+        this.frete = frete;
+    }
+
+    private double getFrete() {
+        return this.frete;
+    }
+
+    // MÉTODOS DOS DIAGRAMAS (D.E e D.S)
+
+
+    // MÉTODOS DE VALIDAÇÃO
+
+    private void validarId(String id) {
+
+    }
+
+    private void validarSituacao(String situacao) {
+
+    }
+
+    private void validarDtSaida(Date dtSaida) {
+
+    }
+
+    private void validarDtChegada(Date dtChegada) {
+
+    }
+
+    private void validarFrete(double frete) {
+
+    }
+
 }
